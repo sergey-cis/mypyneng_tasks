518f884d673d411cec1a0eef1207a7cec019dda0
sergey-cis <ksacertain@gmail.com>
2024-01-20 10:29:38 +0200

    First update

--- exercises/01_intro/task_01.py
+++ exercises/01_intro/task_01.py
@@ -10,5 +10,6 @@ Ruby is a high-level, interpreted, general-purpose programming language.
 При цьому не можна змінювати рядок start_data вручну, тільки за допомогою Python.
 """
 start_data = "Python is a high-level, interpreted, general-purpose programming language."
+start_data.replace("Python is a high-level, interpreted, general-purpose programming language.", "$ python task_01.py \nRuby is a high-level, interpreted, general-purpose programming language.")
 print(start_data)
 
